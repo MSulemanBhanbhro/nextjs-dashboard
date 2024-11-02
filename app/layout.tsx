bda5235c5457aad18c871b2159549b50ecb3d166
M Suleman <ms5901562@gmail.com>
2024-11-02 02:07:52 -0700

    completed nextjs dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'NextJS Dashboard website - Made By M Suleman.',
+  metadataBase: new URL('https://nextjs-dashboard-by-msuleman.vercel.app/'),
+};
  
 export default function RootLayout({
   children,
